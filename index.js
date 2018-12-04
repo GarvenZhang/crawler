4766cd3434b651e6b82f693390c4fd7406853da0
931923959 <931923959@qq.com>
2018-12-04 15:33:43 +0800

    删除index.html

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const fs = require('fs')
 // 只考虑https情况
 
 init({
-  dirPath: path.resolve('../../h5_games/800028'),
-  target: 'https://games.cdn.famobi.com/html5games/h/hex-blitz/v360/index.html?fg_domain=play.famobi.com&fg_aid=A1000-1&fg_uid=6d9f5f75-2f84-44c5-9cfe-06056d557639&fg_pid=4638e320-4444-4514-81c4-d80a8c662371&fg_beat=354&original_ref=',
+  dirPath: path.resolve('../../h5_games/800031'),
+  target: 'https://games.cdn.famobi.com/html5games/p/perfect-piano/v090/?fg_domain=play.famobi.com&fg_aid=A1000-1&fg_uid=24f835e0-d21a-47e1-a721-9f68beed83f3&fg_pid=4638e320-4444-4514-81c4-d80a8c662371&fg_beat=297&original_ref=',
 })
