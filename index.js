e07a18959e80272ec255dda377752531237dd50a
931923959 <931923959@qq.com>
2019-01-12 13:32:46 +0800

    v1.1 准备https.get 换为 request

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path')
 // 只考虑https情况
 
 init({
-  dirPath: path.resolve('../../h5_games/000/801/004'),
+  dirPath: path.resolve('./h5_games/000/801/004'),
   target: 'https://www.yiv.com/games/Ultimate-Boxing/index.html',
   domainBlackList: [
     'google',
